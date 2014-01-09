1db1b103d9b295c961045141f57fc6ac20616c74
Hack Reactor Students <students@hackreactor.com>
2014-01-08 17:59:46 -0800

    Completed all four instantiation types

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,26 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.len = 0;
+  this.storage = {};
+  this.firstIndex = 0;
+  this.count = 0;
 };
+
+Queue.prototype.size = function() {
+  return this.len;
+}
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.len++;
+  this.count++;
+}
+Queue.prototype.dequeue = function() {
+  if(this.len) {
+    var result = this.storage[this.firstIndex];
+    this.len--;
+    this.firstIndex++;
+  }
+  return result;
+}
+
+var NewQueue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.len = 0;
 };
+
+Stack.prototype.size = function(){
+  return this.len;
+};
+Stack.prototype.push = function(value){
+  this.len++;
+  this.storage[this.len] = value;
+};
+Stack.prototype.pop = function(){
+  if (this.len){
+    var result = this.storage[this.len];
+    this.len--;
+  }
+  return result;
+};
+
+var NewStack = new Stack();
