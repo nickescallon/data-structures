b6846190ec6c85d0124a7676e6f446074749a7eb
Hack Reactor Students <students@hackreactor.com>
2014-01-09 11:12:53 -0800

    Hacky solution to linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,39 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-
+  var count = 0
+  var lowestCount = 0;
+  
   list.addToTail = function(value){
+    var results = makeNode(value);
+    if (!this.head){
+      this.head = results;
+    } else {
+      this.tail.next = results;
+      this[count] = this.tail;
+      count++;
+    }
+
+    this.tail = results;
   };
 
   list.removeHead = function(){
+    if (this.head) {
+      var temp = this.head.next;
+      console.log(this.head);
+      delete this[lowestCount];
+      lowestCount++;
+      this.head = temp;
+    }
   };
 
   list.contains = function(target, node){
+    for (var key in this) {
+      if (this[key].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
