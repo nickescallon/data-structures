09c37037b9816cab57c735561ba8048ff7a233ff
Hack Reactor Students <students@hackreactor.com>
2014-01-09 14:25:29 -0800

    Completed Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,9 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+
   return newTree;
 };
 
@@ -9,8 +12,24 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children = this.children || [];
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var result = false;
+
+  var search = function(target, tree){
+    if (target === tree.value){
+      result = true;
+    } else if (tree.children){
+      for(var i=0; i<tree.children.length; i++){
+        search(target, tree.children[i]);
+      }
+    }
+  };
+
+  search(target, this);
+  return result;
 };
 
