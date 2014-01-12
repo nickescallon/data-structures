ad9254e254d74d8718d25e71b791747a79f37c3a
Hack Reactor Students <students@hackreactor.com>
2014-01-11 17:48:28 -0800

    Progress on Rebase Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -8,6 +8,9 @@ var makeBinarySearchTree = function(value){
   binarySearchTree.contains = binarySearchTreeMethods.contains;
   binarySearchTree.breadthFirstLog = binarySearchTreeMethods.breadthFirstLog;
 
+  // binarySearchTree.minDepth = 
+  binarySearchTree.maxDepth = 1;
+
   return binarySearchTree;
 };
 
@@ -15,7 +18,73 @@ var makeBinarySearchTree = function(value){
 var binarySearchTreeMethods = {};
 
 binarySearchTreeMethods.insert = function (value) {
-  if (value < this.value){
+  this.maxDepth++;
+  //each time something is inserted
+  //if this is top level 
+  //push to top level array
+
+  var minDepth = Math.ceil(Math.log(this.maxDepth) / Math.log(2));
+  if (this.maxDepth > minDepth * 2){
+    var nodes = [];
+    var child = makeBinarySearchTree(value);
+    nodes.push(child);
+
+    var getAllNodes = function(node){
+      nodes.push(node);
+      if (node.left){
+        getAllNodes(node.left);
+      }
+      if (node.right){
+        getAllNodes(node.right)
+      }
+    };
+    getAllNodes(this);
+
+    var sortBy = function (array, value) {
+      return array.sort(function (a, b) {
+        return a[value] > b[value];
+      });
+    }
+
+    var sortedNodes = sortBy(nodes, "value" );
+
+    var middles = [];
+
+    var insertMiddle = function (someArray, node) {
+      middles.push(node);
+
+
+      var middle = someArray.length / 2;
+      
+      var leftOfMiddle = middle - 1;
+      var rightOfMiddle = middle + 1;
+      var leftArray = someArray.splice(0, leftOfMiddle);
+      
+      insertMiddle(leftArray);
+    }
+
+    this.value = sortedNodes.length / 2;
+
+    create one array with 0 - just before middle
+    create another array i just after middle to end
+    get the middle of each array and call again
+
+
+
+
+
+    
+    // iterate through the tree and push every value to nodes
+    //   - i think write a recursive function to push every node into the nodes array
+
+    // _.sortby(nodes, value)
+
+    // Grab median
+    // //set maxdepth of median to nodes.length
+    //  and recursively insert successive medians 
+
+  }
+  else if (value < this.value){
     if(this.left === null){
       var child = makeBinarySearchTree(value);
       this.left = child;
@@ -30,6 +99,7 @@ binarySearchTreeMethods.insert = function (value) {
       this.right.insert(value);
     }
   }
+
 };
 
 binarySearchTreeMethods.contains = function (value) {
